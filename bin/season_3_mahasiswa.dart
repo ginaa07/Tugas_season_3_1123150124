18406cc2ffb3e72d8d11fd00208b172f14f6131f
ginaa07 <1123150124@global.ac.id>
2025-10-03 14:13:33 +0700

    first season_3_mahasiswa

---
+++ bin/season_3_mahasiswa.dart
@@ -0,0 +1,5 @@
+import 'package:season_3_mahasiswa/season_3_mahasiswa.dart' as season_3_mahasiswa;
+
+void main(List<String> arguments) {
+  print('Hello world: ${season_3_mahasiswa.calculate()}!');
+}
---
+++ test/season_3_mahasiswa_test.dart
@@ -0,0 +1,8 @@
+import 'package:season_3_mahasiswa/season_3_mahasiswa.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
