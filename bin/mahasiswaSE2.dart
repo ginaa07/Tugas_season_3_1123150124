2f63ef8b7ab8e4b7a72e33f7c41a932e14a1637c
ginaa07 <1123150124@global.ac.id>
2025-10-03 15:32:24 +0700

    MahasiswaSE2 Regina

---
+++ bin/mahasiswaSE2.dart
@@ -0,0 +1,109 @@
+class Mahasiswa {
+  // Properties (atribut)
+  String nama;
+  String nim;
+  String jurusan;
+  int angkatan;
+
+  // Constructor
+  Mahasiswa({
+    required this.nama,
+    required this.nim,
+    required this.jurusan,
+    required this.angkatan,
+  });
+
+  // Method
+  void tampilkanData() {
+    print("Nama : $nama");
+    print("NIM : $nim");
+    print("Jurusan : $jurusan");
+    print("Angkatan : $angkatan");
+  }
+}
+
+class AsistenDosen extends Mahasiswa {
+  String mataKuliah;
+
+  AsistenDosen({
+    required String nama,
+    required String nim,
+    required String jurusan,
+    required int angkatan,
+    required this.mataKuliah,
+  }) : super(
+          nama: nama,
+          nim: nim,
+          jurusan: jurusan,
+          angkatan: angkatan,
+        );
+
+  // Override method
+  @override
+  void tampilkanData() {
+    super.tampilkanData();
+    print("Asisten MK: $mataKuliah");
+  }
+}
+
+abstract class Pendaftaran {
+  void daftarMatkul(String matkul);
+}
+
+class MahasiswaAktif extends Mahasiswa implements Pendaftaran {
+  List<String> matkulDiambil = [];
+
+  MahasiswaAktif({
+    required String nama,
+    required String nim,
+    required String jurusan,
+    required int angkatan,
+  }) : super(
+          nama: nama,
+          nim: nim,
+          jurusan: jurusan,
+          angkatan: angkatan,
+        );
+
+  @override
+  void daftarMatkul(String matkul) {
+    matkulDiambil.add(matkul);
+    print("$nama berhasil daftar mata kuliah $matkul");
+  }
+}
+
+void main() {
+  // Buat objek Mahasiswa
+  var mhs = Mahasiswa(
+    nama: "Regina safarina",
+    nim: "1123150124",
+    jurusan: "Teknik Informatika",
+    angkatan: 2023,
+  );
+  mhs.tampilkanData();
+
+  print("-----");
+
+  // Buat objek AsistenDosen
+  var asdos = AsistenDosen(
+    nama: "Ketut Gunawan",
+    nim: "1332711002",
+    jurusan: "Teknik Informatika",
+    angkatan: 2023,
+    mataKuliah: "Mobile Apps",
+  );
+  asdos.tampilkanData();
+
+  print("-----");
+
+  // Buat objek MahasiswaAktif
+  var mhsAktif = MahasiswaAktif(
+    nama: "Regina",
+    nim: "1123150124",
+    jurusan: "Teknik informatika",
+    angkatan: 2023,
+  );
+  mhsAktif.tampilkanData();
+  mhsAktif.daftarMatkul("Mobile Apps");
+  mhsAktif.daftarMatkul("Pemrograman Objek II");
+}
